255de47a893f545d919852e682fc369ea31c0007
Patryk Malinowski <145374860+Patryk-Malinowski@users.noreply.github.com>
2024-12-03 15:54:25 +0000

    Initial commit

---
+++ src/main/java/com/example/project_copilot/ProjectCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCopilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_copilot/ProjectCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
